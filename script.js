497c8209210b89ccbe48aeb3c1097ed8bb36a626
michael moreno <michaelmoreno@MacBook-Pro-7.local>
2023-01-27 14:15:13 +0100

    added function to clear the text form and allow for the user to input numbers and operations in the textform

---
+++ script.js
@@ -0,0 +1,9 @@
+// var textForm = document.getElementsByClassName("textview")
+
+function insert(num) {
+  document.form1.textview.value = document.form1.textview.value + num;
+}
+
+function clearForm() {
+  document.form1.textview.value = "";
+}
